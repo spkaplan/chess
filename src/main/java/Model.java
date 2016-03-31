dd56c1a77549acc950ad7c4ede28d8d265fc0309
Tia Porcincula <porcint@quest-OptiPlex-9020>
2016-03-30 17:18:52 -0700

    Implemented drawBoard and helper functions for TextView class

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -31,4 +31,5 @@ class Model {
     Model getState() {
         return this;
     }
+    
 }
--- src/main/java/TextView.java
+++ src/main/java/TextView.java
@@ -11,6 +11,44 @@ class TextView {
     }
 
     void refresh() {
+    	model.getState();
+    	drawBoard(model.getBoard());
+    	
         System.out.println("The view has called refresh");
+    }
+    
+    void drawBoard(Board board) throws IndexOutsideOfGridException{
+    	for (int row = 0; row < board.GRID_DIMENSION; row++)
+    	{
+    		for(int col = 0; col < board.GRID_DIMENSION; col++)
+    		{
+    			Position currentPosition = new Position(row, col);
+    			Piece currentPiece = board.gridLookup(currentPosition);
+    			System.out.print(getAbbriviation(currentPiece));
+    		}
+    	}
+    }
+    
+    String getAbbriviation(Piece piece){
+    	PieceType pieceType = piece.getType();
+    	switch(pieceType){
+    		case BISHOP: 
+    			return " BI ";
+    		case KING: 
+    			return " KI ";
+    		case KNIGHT: 
+    			return " KN ";
+    		case PAWN: 
+    			return " PA ";
+    		case QUEEN: 
+    			return " QU ";
+    		case ROOK: 
+    			return " RO ";
+    		case NO_PIECE: 
+    			return " .. ";
+    		default: 
+    			return " ";
+    		
+    	}
     }
 }
