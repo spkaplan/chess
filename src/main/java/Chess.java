8c6468d0078f1764641dc08243a33992026256de
Sam Kaplan <samuelpkaplan@gmail.com>
2016-04-01 17:12:58 -0700

    fixed HOTFIX-4 #6

--- src/main/java/Chess.java
+++ src/main/java/Chess.java
@@ -3,20 +3,21 @@ package main.java;
 /**
  * Created by brandon on 3/30/2016 at 4:44 PM as part of the chess project.
  */
-public class Chess {
+public class Chess
+{
+	public static void main(String[] args)
+	{
+		//Instantiate the Model, View, and Controller//
 
-    public static void main() {
-        //Instantiate the Model, View, and Controller//
+		Model model = new Model();
 
-        Model model = new Model();
+		TextController controller = new TextController();
+		controller.setModel(model);
 
-        TextController controller = new TextController();
-        controller.setModel(model);
+		TextView view = new TextView();
+		view.setModel(model);
+		view.setController(controller);
 
-        TextView view = new TextView();
-        view.setModel(model);
-        view.setController(controller);
-
-        model.addObserver(view);
-    }
+		model.addObserver(view);
+	}
 }
