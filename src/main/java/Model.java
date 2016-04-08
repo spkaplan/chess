8e73d6e60b2d3b7199c3dbdc4b03aa6578a6e59e
Sam Kaplan <samuelpkaplan@gmail.com>
2016-04-07 21:11:47 -0700

    Began work on limiting it so only white can move white pieces and same for black. Took a first stab at a the unit test for this.

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -11,8 +11,8 @@ public class Model extends Observable
 	private Board board;
 	private PieceColor whosTurn;
 	private int turnCount;
-    private List<Position> validMoves;
-    private Exception exceptionThrown;
+	private List<Position> validMoves;
+	private Exception exceptionThrown;
 
 	public Model()
 	{
@@ -21,35 +21,41 @@ public class Model extends Observable
 		this.turnCount = 1;
 	}
 
-    public List<Position> getValidMoves()
-    {
-        return this.validMoves;
-    }
+	public List<Position> getValidMoves()
+	{
+		return this.validMoves;
+	}
 
-    public Exception getExceptionThrown() {
-        return this.exceptionThrown;
-    }
+	public Exception getExceptionThrown()
+	{
+		return this.exceptionThrown;
+	}
 
-    /**
-     * Notify the view and clear the temporary state variables that have been set.
-     */
-    public void notifyObservers() {
-        super.notifyObservers();
-        clearTempStates();
-    }
+	/**
+	 * Notify the view and clear the temporary state variables that have been
+	 * set.
+	 */
+	public void notifyObservers()
+	{
+		super.notifyObservers();
+		clearTempStates();
+	}
 
-    /**
-     * Clear any temporary state variables that have developed since the last call to refresh view.
-     */
-    public void clearTempStates() {
-        this.exceptionThrown = null;
-        this.validMoves = null;
-    }
+	/**
+	 * Clear any temporary state variables that have developed since the last
+	 * call to refresh view.
+	 */
+	public void clearTempStates()
+	{
+		this.exceptionThrown = null;
+		this.validMoves = null;
+	}
 
-    public void setExceptionThrown(Exception exceptionThrown) {
-        this.exceptionThrown = exceptionThrown;
-        setChanged();
-    }
+	public void setExceptionThrown(Exception exceptionThrown)
+	{
+		this.exceptionThrown = exceptionThrown;
+		setChanged();
+	}
 
 	public Board getBoard()
 	{
@@ -66,31 +72,46 @@ public class Model extends Observable
 		return this.turnCount;
 	}
 
-    /**
-     * Moves a piece on the board by interacting with the board. Also, notifies the observers and triggers a view refresh
-     * @param curPos the position of the piece you want to move
-     * @param newPos the position you are attempting to move the piece to
-     */
-	void movePiece(Position curPos, Position newPos)
-    {
-        try {
-            board.movePiece(curPos, newPos);
-        } catch (InvalidPositionException e) {
-            exceptionThrown = e;
-        }
-        setChanged();
-    }
+	/**
+	 * Moves the piece at the current location to the new location.
+	 * 
+	 * @param curPos The position of the piece you want to move.
+	 * @param newPos The position you are attempting to move the piece to.
+	 */
+	public void movePiece(Position curPos, Position newPos)
+	{
+		Piece pieceToMove = board.gridLookup(curPos);
 
-    /**
-     * Collects a list of valid moves from the board and updates the valid moves variable. Then, it notifies the
-     * observers triggering a view refresh. Finally, clearing validmoves so that it's null next time the model
-     * is observed.
-     * @param position the position the piece the user requested valid moves for
-     */
-    void getValidNewPositions(Position position)
-    {
-        List<Position> validNewPositions = board.getValidNewPositions(position);
-        validMoves = validNewPositions;
-        setChanged();
-    }
+		if (pieceToMove.getColor() == this.whosTurn)
+		{
+			try
+			{
+				board.movePiece(curPos, newPos);
+			} catch (InvalidPositionException e)
+			{
+				this.exceptionThrown = e;
+			}
+		} else
+		{
+			String msg = "Cannot move piece of the other color";
+			this.exceptionThrown = new IllegalArgumentException(msg);
+		}
+
+		setChanged();
+	}
+
+	/**
+	 * Collects a list of valid moves from the board and updates the valid moves
+	 * variable. Then, it notifies the observers triggering a view refresh.
+	 * Finally, clearing validmoves so that it's null next time the model is
+	 * observed.
+	 * 
+	 * @param position the position the piece the user requested valid moves for
+	 */
+	void getValidNewPositions(Position position)
+	{
+		List<Position> validNewPositions = this.board.getValidNewPositions(position);
+		this.validMoves = validNewPositions;
+		setChanged();
+	}
 }
