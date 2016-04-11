d21b12b562062f3d11f51d91483b5a2878431cd2
Sam Kaplan <samuelpkaplan@gmail.com>
2016-04-10 18:52:10 -0700

    changed tabs to spaces.

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -8,109 +8,109 @@ import java.util.Observable;
  */
 public class Model extends Observable
 {
-	private Board board;
-	private PieceColor whosTurn;
-	private int turnCount;
-	private List<Position> validMoves;
-	private Exception exceptionThrown;
+    private Board board;
+    private PieceColor whosTurn;
+    private int turnCount;
+    private List<Position> validMoves;
+    private Exception exceptionThrown;
 
-	public Model()
-	{
-		this.board = new Board();
-		this.whosTurn = PieceColor.WHITE;
-		this.turnCount = 1;
-	}
+    public Model()
+    {
+        this.board = new Board();
+        this.whosTurn = PieceColor.WHITE;
+        this.turnCount = 1;
+    }
 
-	public List<Position> getValidMoves()
-	{
-		return this.validMoves;
-	}
+    public List<Position> getValidMoves()
+    {
+        return this.validMoves;
+    }
 
-	public Exception getExceptionThrown()
-	{
-		return this.exceptionThrown;
-	}
+    public Exception getExceptionThrown()
+    {
+        return this.exceptionThrown;
+    }
 
-	/**
-	 * Notify the view and clear the temporary state variables that have been
-	 * set.
-	 */
-	public void notifyObservers()
-	{
-		super.notifyObservers();
-		clearTempStates();
-	}
+    /**
+     * Notify the view and clear the temporary state variables that have been
+     * set.
+     */
+    public void notifyObservers()
+    {
+        super.notifyObservers();
+        clearTempStates();
+    }
 
-	/**
-	 * Clear any temporary state variables that have developed since the last
-	 * call to refresh view.
-	 */
-	public void clearTempStates()
-	{
-		this.exceptionThrown = null;
-		this.validMoves = null;
-	}
+    /**
+     * Clear any temporary state variables that have developed since the last
+     * call to refresh view.
+     */
+    public void clearTempStates()
+    {
+        this.exceptionThrown = null;
+        this.validMoves = null;
+    }
 
-	public void setExceptionThrown(Exception exceptionThrown)
-	{
-		this.exceptionThrown = exceptionThrown;
-		setChanged();
-	}
+    public void setExceptionThrown(Exception exceptionThrown)
+    {
+        this.exceptionThrown = exceptionThrown;
+        setChanged();
+    }
 
-	public Board getBoard()
-	{
-		return this.board;
-	}
+    public Board getBoard()
+    {
+        return this.board;
+    }
 
-	public PieceColor getWhosTurn()
-	{
-		return this.whosTurn;
-	}
+    public PieceColor getWhosTurn()
+    {
+        return this.whosTurn;
+    }
 
-	public int getTurnCount()
-	{
-		return this.turnCount;
-	}
+    public int getTurnCount()
+    {
+        return this.turnCount;
+    }
 
-	/**
-	 * Moves the piece at the current location to the new location.
-	 * 
-	 * @param curPos The position of the piece you want to move.
-	 * @param newPos The position you are attempting to move the piece to.
-	 */
-	public void movePiece(Position curPos, Position newPos)
-	{
-		Piece pieceToMove = board.gridLookup(curPos);
+    /**
+     * Moves the piece at the current location to the new location.
+     * 
+     * @param curPos The position of the piece you want to move.
+     * @param newPos The position you are attempting to move the piece to.
+     */
+    public void movePiece(Position curPos, Position newPos)
+    {
+        Piece pieceToMove = board.gridLookup(curPos);
 
-		if (pieceToMove.getColor() != this.whosTurn)
-		{
-			String msg = "Cannot move piece of the other color";
-			this.exceptionThrown = new IllegalArgumentException(msg);
-			setChanged();
-			return;
-		}
-		try
-		{
-			board.movePiece(curPos, newPos);
-		} catch (InvalidPositionException e)
-		{
-			this.exceptionThrown = e;
-		}
-		setChanged();
-	}
+        if (pieceToMove.getColor() != this.whosTurn)
+        {
+            String msg = "Cannot move piece of the other color";
+            this.exceptionThrown = new IllegalArgumentException(msg);
+            setChanged();
+            return;
+        }
+        try
+        {
+            board.movePiece(curPos, newPos);
+        } catch (InvalidPositionException e)
+        {
+            this.exceptionThrown = e;
+        }
+        setChanged();
+    }
 
-	/**
-	 * Collects a list of valid moves from the board and updates the valid moves
-	 * variable. Then, it notifies the observers triggering a view refresh.
-	 * Finally, clearing validmoves so that it's null next time the model is
-	 * observed.
-	 * 
-	 * @param position the position the piece the user requested valid moves for
-	 */
-	void getValidNewPositions(Position position)
-	{
-		List<Position> validNewPositions = this.board.getValidNewPositions(position);
-		this.validMoves = validNewPositions;
-		setChanged();
-	}
+    /**
+     * Collects a list of valid moves from the board and updates the valid moves
+     * variable. Then, it notifies the observers triggering a view refresh.
+     * Finally, clearing validmoves so that it's null next time the model is
+     * observed.
+     * 
+     * @param position the position the piece the user requested valid moves for
+     */
+    void getValidNewPositions(Position position)
+    {
+        List<Position> validNewPositions = this.board.getValidNewPositions(position);
+        this.validMoves = validNewPositions;
+        setChanged();
+    }
 }
--- src/test/java/ModelTest.java
+++ src/test/java/ModelTest.java
@@ -11,30 +11,30 @@ import org.slf4j.LoggerFactory;
 
 public class ModelTest
 {
-	private static final Logger logger = LoggerFactory.getLogger(ModelTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(ModelTest.class);
 
-	/**
-	 * When it is white's turn, a black piece cannot be moved.
-	 */
-	@Test()
-	public void testCantMovePieceOfOtherColor()
-	{
-		Model model = new Model();
+    /**
+     * When it is white's turn, a black piece cannot be moved.
+     */
+    @Test()
+    public void testCantMovePieceOfOtherColor()
+    {
+        Model model = new Model();
 
-		Position currPosition = null;
-		Position newPosition = null;
-		try
-		{
-			currPosition = new Position(1, 0);
-			newPosition = new Position(2, 0);
-		} catch (InvalidPositionException ex)
-		{
-			logger.error(ex.getMessage());
-		}
+        Position currPosition = null;
+        Position newPosition = null;
+        try
+        {
+            currPosition = new Position(1, 0);
+            newPosition = new Position(2, 0);
+        } catch (InvalidPositionException ex)
+        {
+            logger.error(ex.getMessage());
+        }
 
-		/*White tries to move black piece*/
-		model.movePiece(currPosition, newPosition);
+        /*White tries to move black piece*/
+        model.movePiece(currPosition, newPosition);
 
-		assertTrue(model.getExceptionThrown() instanceof IllegalArgumentException);
-	}
+        assertTrue(model.getExceptionThrown() instanceof IllegalArgumentException);
+    }
 }
